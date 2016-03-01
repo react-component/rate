08204d4f235ab3c7195e83123160116e3838c453
yiminghe <yiminghe@gmail.com>
2016-03-01 15:25:10 +0800

    update demo

--- examples/simple.js
+++ examples/simple.js
@@ -3,8 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../assets/index.less';
 
+function onChange(v) {
+  console.log('selected star', v);
+}
+
 ReactDOM.render(<div style={{ margin: 100, marginRight: 0 }}>
   <Rate
+    onChange={onChange}
     style={{ fontSize: 40 }}
     allowHalf
   />
